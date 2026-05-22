fe4953a2b3488ed5c57270e8a3e4140b19d19dcd
SenkoSun <212nazik212@gmail.com>
2026-05-22 21:02:24 +0500

    update type User id Integer -> Long

--- src/main/java/com/senkosun/pastebin/entity/User.java
+++ src/main/java/com/senkosun/pastebin/entity/User.java
@@ -18,7 +18,7 @@ import java.time.LocalDateTime;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     @Column(name = "username", unique = true, nullable = false)
     private String username;
