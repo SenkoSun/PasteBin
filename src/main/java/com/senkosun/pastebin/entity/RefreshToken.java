f230a0f7a180d26f08bbff2229046291f045b75b
SenkoSun <212nazik212@gmail.com>
2026-05-22 20:46:41 +0500

    add RefreshToken Entity for work with jwt tokens

---
+++ src/main/java/com/senkosun/pastebin/entity/RefreshToken.java
@@ -0,0 +1,34 @@
+package com.senkosun.pastebin.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "expiry_date", nullable = false)
+    private LocalDateTime expiryDate;
+
+    @Column(nullable = false)
+    private boolean revoked;
+
+}
