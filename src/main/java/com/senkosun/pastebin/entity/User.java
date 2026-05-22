68799eb37f0e834fdbaac87fb0ac70709b1caba0
SenkoSun <212nazik212@gmail.com>
2026-05-22 20:21:18 +0500

    add User entity

---
+++ src/main/java/com/senkosun/pastebin/entity/User.java
@@ -0,0 +1,38 @@
+package com.senkosun.pastebin.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "is_admin", nullable = false)
+    private boolean isAdmin;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+}
