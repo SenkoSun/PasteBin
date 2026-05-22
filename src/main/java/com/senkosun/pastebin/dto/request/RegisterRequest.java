8796cb4ff827bb90f06399ed27972dee7ab752b5
SenkoSun <212nazik212@gmail.com>
2026-05-22 22:07:08 +0500

    add Register request

---
+++ src/main/java/com/senkosun/pastebin/dto/request/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.senkosun.pastebin.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "The username must have at least 6 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 100, message = "The password must have at least 6 characters")
+    private String password;
+}
